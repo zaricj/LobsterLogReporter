8e17a110ad6da5bc4cc388f621264d95ad718a14
Zaric, Jovan <jovanzaric98@hotmail.com>
2026-04-20 16:03:30 +0200

    Update

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 """
 Every single pattern key in patterns.json must contain a regex with multiple groups that will be used to search in an event block in log file.
-This makes sure that a single line contains all the matches from the same event block
+This makes sure that a single line contains all the matches from the same event block.
+
+'JSON' Expected structure:
+    >>> {
+        "category_name": {
+            "base": {
+                "separator": "regex_string"
+            },
+            "patterns": {
+                "pattern_name": "regex_string",
+                ...
+            }
+        },
+        ...
+    }
 """
 
 from pathlib import Path
 from datetime import datetime
 from modules.core.pipeline import run_pipeline
 
-if __name__ == "__main__":
-    
+def main() -> None:
     # Search patterns config
     PATTERNS_CONFIG = Path("patterns/patterns.json")
-    PATTERN_KEY = "catalina_out_jasperserver"
+    PATTERN_KEY = "http_requests_jasperserver"
     
     # File(s) to search config
-    FILE_PATTERN = "*.out"
+    FILE_PATTERN = "*.txt"
     FILES_DIR = Path("logs")
 
     # CSV output
     OUTPUT_DIR = Path("output")
     TIMESTAMP_PREFIX = datetime.now().strftime("%d_%m_%Y_%H%M%S")
     CSV_FILE = OUTPUT_DIR / f"Result_{TIMESTAMP_PREFIX}.csv"
-    
 
     run_pipeline(
         patterns_config=PATTERNS_CONFIG,
@@ -32,3 +44,7 @@ if __name__ == "__main__":
         event_keyword="",
         show_progress=True
     )
+
+if __name__ == "__main__":
+    main()
+
